15c9d4d042008c37c188daa9fe7f1ba27d7ffe59
Raul Ferrando <rferrando@okode.com>
2024-01-14 13:36:27 +0100

    update config

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -35,5 +35,11 @@ return packer.startup(function(use)
 
     use { "lewis6991/gitsigns.nvim" }
 
+    use { "nvim-lualine/lualine.nvim" }
+
+    use {
+        'ruifm/gitlinker.nvim',
+        requires = 'nvim-lua/plenary.nvim',
+    }
 
 end)
--- set.sh
+++ set.sh
@@ -1,3 +1,5 @@
-#!/bin/zsh
+#!/bin/bash
 
-ln -s $(pwd)/nvim ~/.config/nvim
+[ ! -d "$HOME/.config/nvim" ] && ln -s $(pwd)/nvim ~/.config/nvim
+[ ! -d "$HOME/.config/yabai" ] && ln -s $(pwd)/yabai ~/.config/yabai
+[ ! -d "$HOME/.config/alacritty" ] && ln -s $(pwd)/alacritty ~/.config/alacritty
