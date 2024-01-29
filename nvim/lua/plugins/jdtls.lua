a7fb8cd83c2fbe11ccc072302a1b5c58287be19e
Raul Ferrando <rferrandop@protonmail.com>
2024-01-29 23:35:41 +0100

    xdddd

---
+++ nvim/lua/plugins/jdtls.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "mfussenegger/nvim-jdtls",
+    ---@type lspconfig.options.jdtls
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+      jdtls = function(opts)
+        local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
+        local jvmArg = "-javaagent:" .. install_path .. "/lombok.jar"
+        table.insert(opts.cmd, "--jvm-arg=" .. jvmArg)
+        table.insert(opts.cmd, "--jvm-arg=" .. "-Xmx2G")
+        return opts
+      end,
+    },
+  },
+}
