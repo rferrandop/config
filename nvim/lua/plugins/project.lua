86a916582c720b459531e30c3edaf6189e3d59a3
Raul Ferrando <rferrandop@protonmail.com>
2024-02-11 20:51:29 +0100

    d

---
+++ nvim/lua/plugins/project.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "ahmedkhalf/project.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.manual_mode = false
+      opts.detection_methods = { "lsp", "pattern" }
+      opts.patterns = {
+        ".git",
+        ".hg",
+        ".svn",
+      }
+    end,
+  },
+}
